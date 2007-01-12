4b6a12ee172688770d908b72015f5a6ccb683721
Katarína Machálková <kmachalkova@suse.cz>
2007-01-12 15:10:18 +0000

    - Added support for ldap-dhcp-server-cn configuration option to   get/set alternative name for dhcpServer LDAP object (FaTE #227) - 2.13.11
    svn path=/branches/SuSE-SLE-10-SP1-Branch/dhcp-server/; revision=35304

--- src/DhcpServer.pm
+++ src/DhcpServer.pm
@@ -94,6 +94,8 @@ my @original_allowed_interfaces = ();
 
 my $dns_server_available = 0;
 
+my $ldap_dhcp_server_cn = "";
+
 YaST::YCP::Import ("SCR");
 YaST::YCP::Import ("CWMTsigKeys");
 YaST::YCP::Import ("DNS");
@@ -884,7 +886,7 @@ sub PrepareToSave {
 sub FindEntry {
     my $self = shift;
     my $type = shift;
-    my $id = shift;
+    my $id = shift || "";
 
     my $index = -1;
     my $found = -1;
@@ -2319,6 +2321,23 @@ sub LdapInit {
 	    $configured_ldap = 1;
 	}
     }
+
+    # check if there is ldap-dhcp-server-cn entry in config file
+    # even if LDAP is not in use
+    if (defined $settings{"ldap-dhcp-server-cn"}) {
+	$ldap_dhcp_server_cn = $settings{"ldap-dhcp-server-cn"} || "";
+	# removing quotes
+	# format: ldap-dhcp-server-cn "entry"
+	if ($ldap_dhcp_server_cn =~ /^\".*\"$/) {
+	    $ldap_dhcp_server_cn =~ s/(^\"|\"$)//g;
+	    $ldap_dhcp_server_cn =~ s/\\\"/\"/g;
+	}
+	y2milestone ("ldap-dhcp-server-cn defined (".$ldap_dhcp_server_cn.")");
+    } else {
+	$ldap_dhcp_server_cn = "";
+	y2milestone ("ldap-dhcp-server-cn not defined");
+    }
+
     y2milestone ("DHCP configured LDAP: $configured_ldap");
 
     # grab info about the LDAP server
@@ -2457,11 +2476,6 @@ sub LdapInit {
 	    $_->{"key"} =~ /^[ \t]*ldap-.*$/;
 	} @settings_for_ldap;
 
-	# check if there is ldap-dhcp-server-cn entry in config file
-	my @ldap_dhcp_server_entry = grep {
-	    $_->{"key"} eq "ldap-dhcp-server-cn";
-	} @settings_for_ldap;
-	my $ldap_dhcp_server_cn = $ldap_dhcp_server_entry[0]{'value'};
 	my $filter = "(&(objectClass=dhcpServer)";
 
 	# if ldap-dhcp-server-cn is found, adjust filter expression
@@ -2543,6 +2557,22 @@ sub LdapInit {
 	    }
 	}
     }
+}
+
+BEGIN { $TYPEINFO{GetLdapDHCPServerCN} = ["function", "string"];}
+sub GetLdapDHCPServerCN {
+    if (defined $ldap_dhcp_server_cn) {
+	return $ldap_dhcp_server_cn;
+    }
+    return "";
+}
+
+BEGIN { $TYPEINFO{SetLdapDHCPServerCN} = ["function", "void", "string"];}
+sub SetLdapDHCPServerCN {
+    my $self = shift;
+    my $new_value = shift || "";
+
+    $ldap_dhcp_server_cn = $new_value;
 }
 
 BEGIN { $TYPEINFO{LdapPrepareToWrite} = ["function", "boolean"];}
